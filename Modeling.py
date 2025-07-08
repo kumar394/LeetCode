40cef58cf97df764e87ad6c2aab405814c6cc081
Shivendra Kumar <shivendra8960@gmail.com>
2025-07-09 03:15:31 +0530

    dbt deployment code

--- Modeling.py
+++ Modeling.py
@@ -1,3 +1,17 @@
 # This file has modelling codes that will be used in model building
-#
 
+# The following code is load/deploy model binary file on DBT platform
+{{
+    config(
+        materialized = 'model',
+        tags = ['dbt_tag'],
+        ml_config = {
+            "model_type": "xgboost",
+            "model_path": "cloud_location"
+        },
+    )
+}}
+
+# The following code is to generate scores from the model file
+SELECT predicted_label AS model_score
+FROM {{dbt_ml.predict(ref('dbt_model_name'), ref('data_table')) }}
